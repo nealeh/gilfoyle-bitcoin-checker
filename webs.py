17fdf01c36b7100551da8b313de3ce64f6ba50da
Neale Harper <neale@neales-air.home>
2022-08-03 21:20:20 +0100

    tidy up

--- webs.py
+++ webs.py
@@ -11,11 +11,6 @@ ws = create_connection(URL)
 
 params = {"type": "subscribe", "product_ids": ["BTC-USD"],
 "channels": [{"name": "ticker", "product_ids": ["BTC-USD"]}]}
-
-# {'type': 'ticker', 'sequence': 22868951000, 'product_id': 'BTC-USD', 'price': '58193.25', 'open_24h': '54839.16', 
-# 'volume_24h': '22151.57398753', 'low_24h': '54138.01', 'high_24h': '59576.18', 'volume_30d': '734284.99591688', 
-# 'best_bid': '58184.50', 'best_ask': '58193.25', 'side': 'buy', 'time': '2021-03-18T11:48:34.775847Z', 
-# 'trade_id': 146606993, 'last_size': '0.0021167'}
     
 try:
     while True:
@@ -26,7 +21,6 @@ try:
             btc_price = float(converted['price'])
             date = dateutil.parser.isoparse(converted['time'])
             print(btc_price, date)
-            playsound('You_Suffer.mp3')
             if 'open_24' in converted:
                 opening_price = float(converted['open_24h'])
                 ten_percent = opening_price * 0.10
